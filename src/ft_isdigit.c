90fba64ddfe549e891318b4a7a72ca64e6e273f1
Lula <lulacode@gmail.com>
2022-09-07 19:25:11 +0200

    isdigit tested

--- src/ft_isdigit.c
+++ src/ft_isdigit.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lphelipe <lphelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/04 23:37:42 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/09/07 19:19:17 by lphelipe         ###   ########.fr       */
+/*   Created: 2022/09/07 19:20:07 by lphelipe          #+#    #+#             */
+/*   Updated: 2022/09/07 19:24:01 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-void	*ft_memset(void *b, int c, size_t len);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-#endif
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (2048);
+	return (0);
+}
--- tests/tests_libft.c
+++ tests/tests_libft.c
@@ -112,6 +112,34 @@ MU_TEST_SUITE(test_isalpha_receiving_minus_65_returns_false)
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
+MU_TEST_SUITE(test_isdigit_receiving_1_returns_true)
+{
+	//ARRANGE
+	int		input = '1';
+	int		expected_result = 2048;// true
+	int		actual_result;
+
+	//ACT
+	actual_result = ft_isdigit(input);
+
+	//ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST_SUITE(test_isdigit_receiving_a_returns_false)
+{
+	//ARRANGE
+	int		input = 'a';
+	int		expected_result = 0;// true
+	int		actual_result;
+
+	//ACT
+	actual_result = ft_isdigit(input);
+
+	//ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
 MU_TEST_SUITE(test_suite) {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 	
@@ -122,6 +150,8 @@ MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_isalpha_receiving_char_1_returns_false);
 	MU_RUN_TEST(test_isalpha_receiving_asterisc_returns_false);
 	MU_RUN_TEST(test_isalpha_receiving_minus_65_returns_false);
+	MU_RUN_TEST(test_isdigit_receiving_1_returns_true);
+	MU_RUN_TEST(test_isdigit_receiving_a_returns_false);
 }
 
 int main() {
