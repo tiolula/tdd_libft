55c18a6d11b985e05e225918db6085030e2c9fc6
Felipe Peternella <felipevex@gmail.com>
2022-09-10 20:33:21 -0300

    bash script to run everything!

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m' # No Color
+
+cd tests && \
+make && \
+make leak && \
+cd ../src && \
+make && \
+norminette && \
+echo ""  && \
+echo -e "${GREEN}SUCESSO!${NO_COLOR}" 
