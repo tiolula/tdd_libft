7a632fb65ec3f6e2a03e12236c1bb9e3b20dc3b1
Felipe Peternella <felipevex@gmail.com>
2022-09-10 22:19:45 -0300

    refactoring ft_bzero to use ft_memset

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,22 +6,15 @@
 /*   By: lphelipe <lphelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 22:58:06 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/09/10 22:59:37 by lphelipe         ###   ########.fr       */
+/*   Updated: 2022/09/11 01:18:30 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+void	*ft_memset(void *b, int c, size_t len);
+
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*typed_s;
-	size_t			i;
-
-	typed_s = ((unsigned char *)s);
-	i = 0;
-	while (i < n)
-	{
-		typed_s[i] = '\0';
-		i++;
-	}
+	ft_memset(s, 0, n);
 }
