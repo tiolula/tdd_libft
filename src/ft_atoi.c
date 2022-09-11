6ba0f928f3754afa6649818b40e5de7e0ec6990d
Felipe Peternella <felipevex@gmail.com>
2022-09-11 11:20:41 -0300

    optimize ft_atoi code

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lphelipe <lphelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 23:46:56 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/09/11 03:10:20 by lphelipe         ###   ########.fr       */
+/*   Updated: 2022/09/11 14:18:17 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,21 @@ int	helper_is_space(const char c)
 	return (0);
 }
 
+int	helper_get_negative_and_forward_pointer(const char v, const char **ptr)
+{
+	if (v == '-')
+	{
+		(*ptr)++;
+		return (-1);
+	}
+	else if (v == '+')
+	{
+		(*ptr)++;
+		return (1);
+	}
+	return (1);
+}
+
 int	ft_atoi(const char *c)
 {
 	int	result;
@@ -32,22 +47,12 @@ int	ft_atoi(const char *c)
 
 	result = 0;
 	i = 0;
-	negative = 1;
 	while (helper_is_space(c[0]))
 		c++;
-	if (c[i] == '-' || c[i] == '+')
+	negative = helper_get_negative_and_forward_pointer(c[0], &c);
+	while (ft_isdigit(c[i]))
 	{
-		if (c[i] == '-')
-			negative = -1;
-		c++;
-	}
-	while (c[i])
-	{
-		if (!ft_isdigit(c[i]))
-			break ;
-		if (i > 0)
-			result *= 10;
-		result += (c[i] - 48);
+		result = (result * 10) + (c[i] - 48);
 		i++;
 	}
 	return (result * negative);
