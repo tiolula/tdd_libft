6a09c8d3829e59225d5775d9dbad531bfc13d1ce
Lula <lulacode@gmail.com>
2022-09-15 01:20:40 +0200

    isprint done

--- src/ft_isprint.c
+++ src/ft_isprint.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lphelipe <lphelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/04 23:37:42 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/09/15 01:15:21 by lphelipe         ###   ########.fr       */
+/*   Created: 2022/09/15 01:15:40 by lphelipe          #+#    #+#             */
+/*   Updated: 2022/09/15 01:20:24 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
-
-void	*ft_memset(void *b, int c, size_t len);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int		ft_toupper(int c);
-void	ft_bzero(void *s, size_t n);
-int		ft_atoi(const char *c);
-char	*ft_itoa(int n);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-#endif
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (16384);
+	return (0);
+}
--- tests/tests_libft.c
+++ tests/tests_libft.c
@@ -773,11 +773,95 @@ MU_TEST(test_isascii_entering_127_returns_true)
 {
 	// ARRANGE
 	int c = 127;
-	int	expected_result = 1; //false
+	int	expected_result = 1; //true
 	int	actual_result;
 
 	// ACT
 	actual_result = ft_isascii(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_a_returns_true)
+{
+	// ARRANGE
+	int c = 'a';
+	int	expected_result = 16384; //true
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_space_returns_true)
+{
+	// ARRANGE
+	int c = ' ';
+	int	expected_result = 16384; //true
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_del_returns_false)
+{
+	// ARRANGE
+	int c = 127;
+	int	expected_result = 0; //false
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_tab_returns_false)
+{
+	// ARRANGE
+	int c = '\t';
+	int	expected_result = 0; //false
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_0_returns_false)
+{
+	// ARRANGE
+	int c = 0;
+	int	expected_result = 0; //false
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isprint_entering_126_returns_true)
+{
+	// ARRANGE
+	int c = 126;
+	int	expected_result = 16384; //false
+	int	actual_result;
+
+	// ACT
+	actual_result = ft_isprint(c);
 
 	// ASSERT
 	mu_assert_int_eq(expected_result, actual_result);
@@ -846,6 +930,13 @@ MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_isascii_entering_0_returns_true);
 	MU_RUN_TEST(test_isascii_entering_129_returns_false);
 	MU_RUN_TEST(test_isascii_entering_127_returns_true);
+
+	MU_RUN_TEST(test_isprint_entering_a_returns_true);
+	MU_RUN_TEST(test_isprint_entering_space_returns_true);
+	MU_RUN_TEST(test_isprint_entering_del_returns_false);
+	MU_RUN_TEST(test_isprint_entering_tab_returns_false);
+	MU_RUN_TEST(test_isprint_entering_0_returns_false);
+	MU_RUN_TEST(test_isprint_entering_126_returns_true);
 }
 
 int main() {
