2d7405f6d9d62c581e9833da5b17cf9e5a4b8075
Felipe Peternella <felipevex@gmail.com>
2022-09-25 18:41:20 -0300

    adding assert_char using assert_int as a base, but printing %c instead of %i

--- tests/minunit.h
+++ tests/minunit.h
@@ -195,6 +195,21 @@ static void (*minunit_teardown)(void) = NULL;
 	}\
 )
 
+#define mu_assert_char_eq(expected, result) MU__SAFE_BLOCK(\
+	int minunit_tmp_e;\
+	int minunit_tmp_r;\
+	minunit_assert++;\
+	minunit_tmp_e = (expected);\
+	minunit_tmp_r = (result);\
+	if (minunit_tmp_e != minunit_tmp_r) {\
+		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, PRINTRED("%s failed:\n\t%s:%d: %c expected but was %c"), __func__, __FILE__, __LINE__, minunit_tmp_e, minunit_tmp_r);\
+		minunit_status = 1;\
+		return;\
+	} else {\
+		printf(BOLDGREEN("."));\
+	}\
+)
+
 #define mu_assert_double_eq(expected, result) MU__SAFE_BLOCK(\
 	double minunit_tmp_e;\
 	double minunit_tmp_r;\
