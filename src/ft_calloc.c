43ba95f8977b22da44dde48412d1e38a2c469474
tiolula <lulacode@gmail.com>
2022-10-27 21:55:24 -0300

    calloc done

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,19 +6,27 @@
 /*   By: lphelipe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 18:55:28 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/10/27 19:21:54 by lphelipe         ###   ########.fr       */
+/*   Updated: 2022/10/27 21:52:01 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+// There is a verification to check if count * size is bigger than size_t.
+// To prevent overflow on this evaluation, we pass 'size' to the other
+// side of the equation.
+// To prevent division by zero, we check it before evaluating.
+// OMG.
 void	*ft_calloc(size_t count, size_t size)
 {
-	void *allocated;
-	printf(" VAI PRINTAAAAAAAAAAAAAAAAAAAAAAAAR\n");
+	void	*allocated;
+	size_t	size_max;
+
+	size_max = ((size_t) ~((size_t) 0));
+	if (size != 0 && count > size_max / size)
+		return (NULL);
 	allocated = malloc(count * size);
-	allocated[0] = 'L';
-	printf("allocated: %s\n", allocated);
-	return allocated;
+	ft_bzero(allocated, count * size);
+	return (allocated);
 }
--- tests/tests_libft.c
+++ tests/tests_libft.c
@@ -2234,6 +2234,7 @@ MU_TEST_SUITE(test_calloc_allocating_1_byte_char)
 
 	//ASSERT
 	mu_assert_char_eq(expected_result, *actual_result);
+	free(actual_result);
 }
 
 MU_TEST_SUITE(test_calloc_allocating_100000_bytes_char)
@@ -2249,6 +2250,27 @@ MU_TEST_SUITE(test_calloc_allocating_100000_bytes_char)
 
 	//ASSERT
 	mu_assert_mem_eq(expected_result, actual_result, count);
+	free(actual_result);
+}
+
+//TODO: FIND A WAY TO SEE THIS TEST FAILING :'( #NOTFAIL
+MU_TEST_SUITE(test_calloc_allocating_more_than_size_t_max_should_return_null)
+{
+	//ARRANGE
+	
+#define SIZE_MAX ((size_t)~((size_t)0))
+
+	size_t	count = SIZE_MAX + 1;
+	size_t	size = sizeof(char);
+	char	*expected_result = NULL;
+	char	*actual_result;
+
+	//ACT
+	actual_result = ft_calloc(count, size);
+
+	//ASSERT
+	mu_assert_mem_eq(expected_result, actual_result, count);
+	free(actual_result);
 }
 
 MU_TEST_SUITE(test_suite) {
@@ -2419,6 +2441,7 @@ MU_TEST_SUITE(test_suite) {
 
 	MU_RUN_TEST(test_calloc_allocating_1_byte_char);
 	MU_RUN_TEST(test_calloc_allocating_100000_bytes_char);
+	MU_RUN_TEST(test_calloc_allocating_more_than_size_t_max_should_return_null);
 }
 
 int main() {
